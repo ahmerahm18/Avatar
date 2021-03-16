297f438263c19455b766043d6459146def44860d
ahmerahm18 <70781541+ahmerahm18@users.noreply.github.com>
2021-03-16 14:39:57 -0700

    Avatar

--- sketch.js
+++ sketch.js
@@ -8,6 +8,7 @@ var p;
 let x;
 let y;
 let move = false;
+let sketchStarted = false;
 let rain = [];
 
 //function creates the canvas
@@ -18,9 +19,7 @@ function setup() {
   //angle
   angleMode(degrees)
 
-  //get mic input and assign it
-  mic = new p5.AudioIn();
-  mic.start();
+  createButton("Start Audio Variables").mousePressed(startSketch);
 
   //set values for variables
   x = width;
@@ -41,67 +40,73 @@ function setup() {
   }
 }
 
-//function draws the avatar
-function draw() {
-  //keeps the background colors constant while eyes and colors are redrawn
-  background(20)
+function startSketch(){
+  //get mic input and assign it
+  mic = new p5.AudioIn();
+  mic.start();
 
-  //call raindrops behind mask
-  //calls the raindrops
-  //change what i is less than in order to change the amount of raindrops
-  for(let i = 0; i < 57; i++){
-    rain[i].display();
-  }
-
-  //made whole image smaller to fit halo
-  push();
-  translate(x*.05, y*.1)
-  scale(.9)
-
-  //calls class neckshape
-  neck.display();
-
-  //calls class headshape
-  head.display();
-
-  //calls class bluemask
-  mask.display();
-
-  //calls class gas
-  gas.rightgas();
-  gas.leftgas();
-
-  //call class for snowflakes
-  push();
-    translate(x*-.17, y*.125);
-    rotate(-.3);
-    snowflake.leftflake();
-  pop();
-
-  push();
-    translate(x*.21, y*-.17);
-    rotate(.3);
-    noStroke();
-    snowflake.rightflake();
-  pop();
-
-  //calls class eyes
-  pupils.leftpupil();
-
-  pupils.rightpupil();
-
-  //calls class halo
-  halo.display();
-
-  //calls the raindrops infront of mask
-  for(let i = 0; i < 3; i++){
-    rain[i].display();
-  }
-
-  //blink
-  // use millis() with if and else statements
+  sketchStarted = true;
 }
 
-function mousePressed(){
-  draw();
+//function draws the avatar
+function draw() {
+  if (sketchStarted == true){
+    //keeps the background colors constant while eyes and colors are redrawn
+    background(20)
+
+    //call raindrops behind mask
+    //calls the raindrops
+    //change what i is less than in order to change the amount of raindrops
+    for(let i = 0; i < 57; i++){
+      rain[i].display();
+    }
+
+    //made whole image smaller to fit halo
+    push();
+    translate(x*.05, y*.1)
+    scale(.9)
+
+    //calls class neckshape
+    neck.display();
+
+    //calls class headshape
+    head.display();
+
+    //calls class bluemask
+    mask.display();
+
+    //calls class gas
+    gas.rightgas();
+    gas.leftgas();
+
+    //call class for snowflakes
+    push();
+      translate(x*-.17, y*.125);
+      rotate(-.3);
+      snowflake.leftflake();
+    pop();
+
+    push();
+      translate(x*.21, y*-.17);
+      rotate(.3);
+      noStroke();
+      snowflake.rightflake();
+    pop();
+
+    //calls class eyes
+    pupils.leftpupil();
+
+    pupils.rightpupil();
+
+    //calls class halo
+    halo.display();
+
+    //calls the raindrops infront of mask
+    for(let i = 0; i < 3; i++){
+      rain[i].display();
+    }
+
+    //blink
+    // use millis() with if and else statements
+  }
 }
